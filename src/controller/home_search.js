04947f4cee960c6953d91464d0ce7fbeca505ea7
duzhaoquan <zhaoquan.du@mxplayer.in>
2023-09-25 20:10:06 +0800

    s

--- src/controller/home_search.js
+++ src/controller/home_search.js
@@ -3,7 +3,7 @@ module.exports = (req, res) => {
 
     const { _label_like } = req.query
     const allData = require('../../data/home_search')()
-    const list = allData.list.filters((v) => v.label.includes(_label_like))
+    const list = allData.list.filter((v) => v.label.includes(_label_like))
     
     setTimeout(() => {
         return res.success({ list })
